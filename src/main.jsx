e42bb19d3e0b76ab11a94c2ad909d55b772ccfa7
ww-ahyuda <wahyuda542@smk.belajar.id>
2024-09-18 16:11:10 +0700

    Setup Redux Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-// src/main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'; // Import Tailwind CSS
-import App from './App.jsx';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux'; // Import Provider dari React-Redux
+import { store } from './app/store'; // Import store Redux
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.render(
+  <React.StrictMode>
+    {/* Bungkus App dengan Provider untuk memberikan akses store Redux */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
